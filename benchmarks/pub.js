eb1574b17b4765c88648361ad6a4f3577e4acd39
Mher Movsisyan <mher.movsisyan@gmail.com>
2013-03-09 21:04:59 +0400

    Adds benchmarks

---
+++ benchmarks/pub.js
@@ -0,0 +1,24 @@
+process.env['NODE_CELERY_DEBUG'] = 0
+
+var celery = require('../celery'),
+    util = require('util');
+
+var client = celery.createClient({
+        CELERY_BROKER_URL: 'amqp://',
+    });
+
+client.on('error', function (err) {
+    console.log(err);
+});
+
+var n = parseInt(process.argv.length > 2 ? process.argv[2] : 1000);
+
+client.once('connect', function() {
+    var start = Date.now();
+    for (var i=0; i<n; i++) {
+        client.call('tasks.add', [i,i]);
+    }
+
+    console.log(util.format('Published %d messages in %s milliseconds',
+                            n, Date.now() - start));
+});
---
+++ benchmarks/pubsub.js
@@ -0,0 +1,30 @@
+process.env['NODE_CELERY_DEBUG'] = 0
+
+var celery = require('../celery'),
+    util = require('util');
+
+var client = celery.createClient({
+        CELERY_BROKER_URL: 'amqp://',
+        CELERY_RESULT_BACKEND: 'amqp',
+    });
+
+client.on('error', function (err) {
+    console.log(err);
+});
+
+var n = parseInt(process.argv.length > 2 ? process.argv[2] : 1000);
+
+client.once('connect', function() {
+    var start = Date.now();
+    var j = n;
+    for (var i=0; i<n; i++) {
+        client.call('tasks.add', [i,i], null, null, function (result) {
+            j--;
+            if (j === 1) {
+                console.log(util.format('Execued %d tasks in %s milliseconds',
+                                        n, Date.now() - start));
+                client.end();
+            }
+        });
+    }
+});
