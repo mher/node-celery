580c6b56213b69b6b00fb088588a773d0f186634
kozehgar <koozehgar@parspack.com>
2017-02-14 16:52:26 +0330

    Conflict with php-celery resolved

--- celery.js
+++ celery.js
@@ -199,7 +199,7 @@ function RedisBackend(conf) {
             }
         });
         // subscribe to redis results
-        self.redis.psubscribe(key_prefix + '*', () => {
+        self.redis.psubscribe(key_prefix + '[^php_]*', () => {
             self.emit('ready');
         });
     });
